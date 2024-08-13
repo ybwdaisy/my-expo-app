8ec39d995f9407eec40d67841e0ad899f0b0f82a
yangbiwan <yangbiwan@lasoplanet.com>
2024-08-13 18:58:54 +0800

    update

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,7 +1,9 @@
 import { useFonts } from 'expo-font';
 import { Link } from 'expo-router';
-import { Text, View, StyleSheet, useColorScheme, Image } from 'react-native';
+import { Text, View, StyleSheet, Image, Button } from 'react-native';
 import { useAssets } from 'expo-asset';
+import * as MySettingsModule from 'my-expo-settings';
+import { useEffect, useState } from 'react';
 
 export default function HomeScreen() {
   const [loaded, fontsError] = useFonts({
@@ -9,18 +11,31 @@ export default function HomeScreen() {
     'Apple Chancery': require('../../../assets/fonts/Apple-Chancery.ttf'),
   })
   const [assets, assetsError] = useAssets([require('../../../assets/images/react.png')])
+  const [theme, setTheme] = useState<MySettingsModule.Theme>(MySettingsModule.getTheme());
 
-  const colorTheme = useColorScheme();
+  useEffect(() => {
+    const subscription = MySettingsModule.addThemeListener(({ theme: newTheme }) => {
+      setTheme(newTheme);
+    });
+    return () => subscription.remove();
+  }, [setTheme]);
 
-  const themeTextStyle = colorTheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
-  const themeContainerStyle = colorTheme === 'light' ? styles.lightContainer : styles.darkContainer;
+  // Toggle between dark and light theme
+  const nextTheme = theme === 'dark' ? 'light' : 'dark';
+
+  // const colorTheme = useColorScheme();
+  const themeTextStyle = theme === 'light' ? styles.lightThemeText : styles.darkThemeText;
+  const themeContainerStyle = theme === 'light' ? styles.lightContainer : styles.darkContainer;
 
   return (
     <View style={[styles.container, themeContainerStyle]}>
       {assets ? <Image style={styles.img} source={assets[0]} /> : null}
-      <Text style={[styles.text, styles.font, themeTextStyle]}>Home</Text>
-      <Link style={[themeTextStyle]} href="/details/1">View first user details</Link>
-      <Link style={[themeTextStyle]} href="/details/2">View second user details</Link>
+      <Link style={themeTextStyle} href="/details/1">View first user details</Link>
+      <Link style={themeTextStyle} href="/details/2">View second user details</Link>
+
+      <Text style={[styles.text, styles.font, themeTextStyle]}>{MySettingsModule.PI}</Text>
+      <Text style={themeTextStyle}>Current Theme is: {MySettingsModule.getTheme()}</Text>
+      <Button title={`Set theme to ${nextTheme}`} onPress={() => MySettingsModule.setTheme(nextTheme)} />
     </View>
   );
 }
